600e1bcac2f437bca6458a7295c5d0598a6f77f1
jose.diaz <jose.diaz@micm.gob.do>
2024-10-21 15:40:15 -0400

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItlaPrimerModulo
+{
+    internal class Calculator
+    {
+        /* To create a Funtion we use public/private + the methods + Name of the Funtion + (declare type of variables) */
+        public int addittion(int A, int B, int maximo)
+        {
+            if (A > B)
+            {
+                maximo = A;
+            }
+        
+                else
+            {
+                maximo = B;
+            }
+            {
+
+            return A + B;
+            {
+                public int substration(int C, int D) 
+
+                { 
+                 return C - D; 
+                              
+                }
+            }
+        }
+
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/* Diseñar un programa que solicite al usuario dos números y, si el primer número es más grande, 
+ calcule y muestre su suma y resta. De lo contrario, calculará y mostrará su producto y cociente.. */
+
+using System;
+
+namespace ItlaPrimerModulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("ingresa tu primer valor numérico: ");
+            int primerNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingresa tu segundo valor numérico: ");
+            int segundoNum = int.Parse(Console.ReadLine());
+
+            int primerResultado = primerNum + segundoNum;
+            Console.WriteLine("La suma de los numeros es: " + primerResultado);
+
+            int segundoResultado = primerNum - segundoNum;
+            Console.WriteLine("La resta de los numeros es " + segundoResultado);    
+            {
+                // Se utiliza el metodo Math.Max para arrojar el valor mas grande
+                Console.WriteLine(Math.Max(primerNum, segundoNum));
+            }
+        }
+
+    }
+}
